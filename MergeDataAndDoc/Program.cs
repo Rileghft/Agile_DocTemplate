904daaecae06374dcbc4ef98d9cc3833e9df1b49
YANG, SHUN-YU <mywonderemailyoung@gmail.com>
2016-03-06 12:02:55 +0800

    完成參數讀取

--- MergeDataAndDoc/Program.cs
+++ MergeDataAndDoc/Program.cs
@@ -9,8 +9,18 @@ namespace MergeDataAndDoc
 {
     class Program
     {
+        static private String dataSrc;
+        static private String templateSrc;
+        static private String outputPath;
         static void Main(string[] args)
         {
+            for(int i = 0; i < args.Length; ++i)
+                Console.WriteLine(args[i]);
+            processArgs(args);
+            Console.WriteLine();
+            Console.WriteLine(dataSrc);
+            Console.WriteLine(templateSrc);
+            Console.WriteLine(outputPath);
             string inputFileName = "defaultInput.txt";
             string outputFileName = "defaultOutput.txt";
             if (args.Length == 2)
@@ -28,6 +38,48 @@ namespace MergeDataAndDoc
                     string outputLine = "***" + line;
                     Console.WriteLine("Write line: " + outputLine);
                     outputFile.WriteLine(outputLine);
+                }
+            }
+        }
+
+        static void processArgs(String[] args)
+        {
+            int state = 0; //1 dataSrc, 2 templateSrc, 3 outputPath
+            for (int i = 0; i < args.Length; ++i)
+            {
+                if (state == 0)
+                    if (args[i][0] == '-')
+                        switch (args[i])
+                        {
+                            case "-i":
+                                state = 1;
+                                break;
+                            case "-t":
+                                state = 2;
+                                break;
+                            case "-r":
+                                state = 3;
+                                break;
+                            default:
+                                break;
+                        }
+                    else
+                        throw new Exception("Invalid argument format.");
+                else
+                {
+                    switch(state)
+                    {
+                        case 1:
+                            dataSrc = args[i];
+                            break;
+                        case 2:
+                            templateSrc = args[i];
+                            break;
+                        case 3:
+                            outputPath = args[i];
+                            break;
+                    }
+                    state = 0;
                 }
             }
         }
